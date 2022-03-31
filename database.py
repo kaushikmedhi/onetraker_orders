b5be74b35cd093580f4b087a65fb15ff58cd8961
Kaushik Medhi <kaushikmedhi97@gmail.com>
2022-03-31 16:11:12 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/OnetrakerAppDatabase"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:admin@54.254.31.24/OnetrakerAppDatabase"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
